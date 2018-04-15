6c82d4eca447ccf24daa2becd33c80d7aa2eddd6
Mettiu <mettiu@gmail.com>
2018-04-15 22:53:04 +0200

    Adoption of command pattern. #SC-2 Fixed Revision ${revision}

--- commands/cd.js
+++ commands/cd.js
@@ -1,10 +1,18 @@
 const { cdCommand } = require('../config');
 const { sendMessage } = require('../utils/send-message');
+const Commander = require('../utils/commander');
 
-async function cd(socket, name) {
+async function action(socket, name) {
   const data = { name };
   const ack = await sendMessage(socket, cdCommand.message, data);
   return JSON.parse(ack);
 }
 
-exports.cd = cd;
+const command = {
+  name: 'cd',
+  action,
+};
+
+Commander.createCommand(command);
+
+module.exports = command;
